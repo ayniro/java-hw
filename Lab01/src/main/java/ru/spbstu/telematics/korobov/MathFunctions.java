b09c0780c756e1a7886e29886efcc23bb5a510e5
Nick Korobov <nickkorobov3d@gmail.com>
2019-11-07 02:32:03 +0300

    kappa

---
+++ Lab01/src/main/java/ru/spbstu/telematics/korobov/MathFunctions.java
@@ -0,0 +1,17 @@
+package ru.spbstu.telematics.korobov;
+
+import org.apache.commons.math3.linear.RealVector;
+
+public class MathFunctions {
+    static double dotProduct(RealVector v1, RealVector v2) {
+        if (v1.getDimension() != v2.getDimension()) {
+            throw new RuntimeException("Vector sizes are not equal");
+        }
+
+        double value = 0.0;
+        for (int i = 0; i < v1.getDimension(); ++i) {
+            value += v1.getEntry(i) * v2.getEntry(i);
+        }
+        return value;
+    }
+}
--- Lab01/src/test/java/ru/spbstu/telematics/korobov/AppTest.java
+++ Lab01/src/test/java/ru/spbstu/telematics/korobov/AppTest.java
@@ -1,33 +1,57 @@
 package ru.spbstu.telematics.korobov;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.apache.commons.math3.linear.ArrayRealVector;
+import org.apache.commons.math3.linear.RealVector;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest extends TestCase {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest(String testName) {
-        super(testName);
+import java.util.Random;
+
+import static ru.spbstu.telematics.korobov.MathFunctions.dotProduct;
+
+public class AppTest {
+    private static final Random rand = new Random();
+    private static final double DOUBLE_COMPARE_EPSILON = 1e-7;
+
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+
+    @Test
+    public void dotProductDimensionsMatch() {
+        for (int i = 0; i < 10000; ++i) {
+            int vectorSize = rand.nextInt(1000);
+            RealVector v1 = getRandomDoublesVector(vectorSize);
+            RealVector v2 = getRandomDoublesVector(vectorSize);
+
+            Assert.assertEquals(v1.dotProduct(v2), dotProduct(v1, v2), DOUBLE_COMPARE_EPSILON);
+        }
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite() {
-        return new TestSuite(AppTest.class);
+    @Test
+    public void dotProductDimensionsMismatch() {
+        int maxVectorSize = 10000;
+
+        for (int i = 0; i < 10000; ++i) {
+            int firstSize = rand.nextInt(maxVectorSize);
+            int secondSize = firstSize;
+            while (secondSize == firstSize) {
+                secondSize = rand.nextInt(maxVectorSize);
+            }
+
+            RealVector v1 = new ArrayRealVector(firstSize);
+            RealVector v2 = new ArrayRealVector(secondSize);
+
+            exception.expect(RuntimeException.class);
+            dotProduct(v1, v2);
+        }
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp() {
-        assertTrue(true);
+    private RealVector getRandomDoublesVector(int size) {
+        double[] array = new double[size];
+        for (int i = 0; i < size; ++i) {
+            array[i] = rand.nextDouble();
+        }
+
+        return new ArrayRealVector(array);
     }
 }
